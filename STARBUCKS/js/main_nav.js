18fd55ffc8a935eb2e00ebc2aba8b7ab067db060
hooninfinity <hooninfinity@naver.com>
2021-06-11 17:36:34 +0900

    star

--- STARBUCKS/js/main_nav.js
+++ STARBUCKS/js/main_nav.js
@@ -1,5 +1,5 @@
-$(window).load(function () {
-  $("#wine-menu-container").click(function () {
-    $(this).toggleClass("xbutton");
-  });
-});
+// $(window).load(function () {
+//   $("#wine-menu-container").click(function () {
+//     $(this).toggleClass("xbutton");
+//   });
+// });
